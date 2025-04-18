4455d46d9fb6cc2198aaacbbca7ca39816c17515
Nut.Thakit <natthakitzn@gmail.com>
2025-04-18 17:37:47 +0700

    feat: complete

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:4000";
 
 const AuthContext = React.createContext();
+
 
 function AuthProvider(props) {
   const [state, setState] = useState({
@@ -8,19 +13,61 @@ function AuthProvider(props) {
     error: null,
     user: null,
   });
-
-  const login = () => {
+  
+  const navigate = useNavigate();
+  const login = async (data) => {
     // 🐨 Todo: Exercise #4
     //  ให้เขียน Logic ของ Function `login` ตรงนี้
     //  Function `login` ทำหน้าที่สร้าง Request ไปที่ API POST /login
     //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
+    // Set state
+    setState({ ...state, loading: true, error: null });
+
+    try {
+      // fetch API form /login
+      const response = await axios.post(`${BASE_URL}/auth/login`, data);
+      
+      // check token
+      if (!response.data.token) {
+        throw new Error("Invalid username or password");
+      }
+
+      // save token to local storage
+      const { token } = response.data;
+      localStorage.setItem("token", token);
+
+      // receive user data and save
+      const userDatafromToken = jwtDecode(token);
+      setState({ ...state, user: userDatafromToken });
+
+      // redirect to home
+      navigate("/");
+    } catch (error) {
+      // handle error
+      setState({ ...state, loading: false, error: error.message });
+      console.error("Login error:", error);
+    }
   };
 
-  const register = () => {
+  const register = async (data) => {
     // 🐨 Todo: Exercise #2
     //  ให้เขียน Logic ของ Function `register` ตรงนี้
     //  Function register ทำหน้าที่สร้าง Request ไปที่ API POST /register
     //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
+    // Set state
+    setState({ ...state, loading: true, error: null });
+
+    try {
+      // fetch API form /register
+      const response = await axios.post(`${BASE_URL}/auth/register`, data);
+
+      // redirect to login
+      navigate("/login");
+    } catch (error) {
+      // handle error
+      setState({ ...state, loading: false, error: error.message });
+      console.error("Register error:", error);
+    }
   };
 
   const logout = () => {
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import { useAuth } from "../contexts/authentication";
 
 function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const { login } = useAuth();
+  
   const handleSubmit = (event) => {
     event.preventDefault();
     // 🐨 Todo: Exercise #4
     //  นำ Function `login` ใน AuthContext มา Execute ใน Event Handler ตรงนี้
+
+    // Execute Function `login`
+    login({ username, password });
   };
 
   return (
